a9d79e9c8a9cc6d502de73d5b46c4656a06e67f5
chiaradesinano <cdesinano@mail.austral.edu.ar>
2025-04-21 08:54:08 -0300

    Update line.py

--- line.py
+++ line.py
@@ -12,12 +12,11 @@ def line():
     print("Para la siguiente ecuación:")
     print(f"\tY = {coef_a}X + {coef_b}")
     print("")
-    
     coef_y_1 = coef_a * coef_x_1 + coef_b
     coef_y_2 = coef_a * coef_x_2 + coef_b
     print("Dados los siguientes puntos:")
     print(f"\tP1 ({coef_x_1}, {coef_y_1})")
     print(f"\tP2 ({coef_x_2}, {coef_y_2})")
     print("")
-    distancia_puntos = ((coef_x_2 - coef_x_1)*2 + (coef_y_2 - coef_y_1)2)*0.5
+    distancia_puntos = ((coef_x_2 - coef_x_1)**2 + (coef_y_2 - coef_y_1)**2)**0.5
     print(f"La distancia entre ellos es: {distancia_puntos}")
